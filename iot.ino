f9c614420de5ca0ebc31a2691f241d1770976523
Jithin Isaac <jithinsaji@gmail.com>
2020-07-25 17:57:49 +0530

    added lines in master

--- iot.ino
+++ iot.ino
@@ -1,16 +1,25 @@
- 
-void setup() { 
-  pinMode(LED_BUILTIN 
-}
- 
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
 
- Hello
- Heyy
-		
-		
+void setup()
+{
+  pinMode(LED_BUILTIN
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+  delay(2000);                     // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+  delay(1000);                     // wait for a second
+
+  Hello
+      Heyy
+          dasdadasdads
+              as da
+                  sd
+                      __GCC_HAVE_SYNC_COMPARE_AND_SWAP_1dsa
+                          sd
+                              a
+
+                                  as das
+                                      d
 }
